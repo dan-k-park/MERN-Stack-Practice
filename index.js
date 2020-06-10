d55f6f346c91d43000661780eb5fd24d53eddfa4
Daniel Park <parkdk96@gmail.com>
2020-06-10 00:35:16 -1000

    cookieparser dependency

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+
+
 mongoose.connect('mongodb+srv://danPark:kAAV.Ss$kB8A8b5@mern-practice.bsft5.mongodb.net/<dbname>?retryWrites=true&w=majority', 
     {
       useNewUrlParser: true,
@@ -9,9 +12,10 @@ mongoose.connect('mongodb+srv://danPark:kAAV.Ss$kB8A8b5@mern-practice.bsft5.mong
     .then(() => console.log('DB connected'))
     .catch(err => console.error(err))
 
-app.get('/', (req, res) => {
-  res.send('hi')
-})
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
 
 app.listen(5000)
 

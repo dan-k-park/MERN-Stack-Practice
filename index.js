883f50367cb1ec5d6783c6aed131f8e99b43b36f
Daniel Park <parkdk96@gmail.com>
2020-06-10 00:57:50 -1000

    register route

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose')
-const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 
+const {User} = require('./models/user');
 
 mongoose.connect('mongodb+srv://danPark:kAAV.Ss$kB8A8b5@mern-practice.bsft5.mongodb.net/<dbname>?retryWrites=true&w=majority', 
     {
@@ -16,6 +17,14 @@ mongoose.connect('mongodb+srv://danPark:kAAV.Ss$kB8A8b5@mern-practice.bsft5.mong
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+
+app.post('/api/users/register', (req, res) => {
+  const user = new User(req.body)
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).json({ success: true })
+  })
+})
 
 app.listen(5000)
 
